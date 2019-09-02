e0adb3b9b4dd8c4a2eedf22570c93a099141c7c7
Kevin Ogden <ogdenkev@gmail.com>
2019-09-02 10:32:28 -0400

    Implements impose resolution function

--- pyscfit/dwells.py
+++ pyscfit/dwells.py
@@ -3,6 +3,7 @@
 import numpy as np
 import scipy.linalg
 
+
 def imposeres(dwells, states, openres, shutres):
     """Impose resolutions for sojourns in an idealized channel recording
 
@@ -23,20 +24,36 @@ def imposeres(dwells, states, openres, shutres):
         Resolved durations
     resolved_states : 1-d array
         States corresponding to the resolved durations
-    unresolved : 1-d array
+    unresolved_inds : 1-d array
         Indices of unresolved durations from the original list of dwell times
     """
 
     TOL = 1e-12
-    # Make sure we start with a resolvable dwell
-    start = 0
-    while (dwells[start] + tol < openres & states[start] != 0) || (dwells(ii)+tol<shutres & states(ii)==0)
-        dwells(ii)=[];
-        states(ii)=[];
-        if isempty(dwells)
-            break
 
-    raise NotImplementedError
+    resolved_mask = (dwells >= openres) & (states != 0)
+    resolved_mask |= (dwells >= shutres) & (states == 0)
+    unresolved_inds = np.nonzero(~resolved_mask)
+
+    resolved_dwells = []
+    resolved_states = []
+    for resolved, sojourns in itertools.groupby(
+        zip(dwells, states, resolved_mask), key=lambda tup: tup[2]
+    ):
+        if resolved:
+            for d, s, r in sojourns:
+                resolved_dwells.append(d)
+                resolved_states.append(s)
+        else:
+            if resolved_dwells:
+                # There has already been a resolved sojourn, so we can add
+                # the unresolved time to the last resolved sojourn
+                sojourn_times, sojourn_states, sojourn_resolved = zip(
+                    *sojourns
+                )
+                unresolved_time = sum(sojourn_times)
+                resolved_dwells[-1] += unresolved_time
+
+    return resolved_dwells, resolved_states, unresolved_inds
 
 
 def concatdwells():
